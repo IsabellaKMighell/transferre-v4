da81d48f7361cbe482776ef6026e3e9ff1523b29
Isabella Mighell <ikm4dm@virginia.edu>
2022-05-26 15:31:37 -0400

    added serializer and routes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :balance, :remainder, :password_digest
+  attributes :id, :username, :balance, :remainder
+
+  def balance
+    expenses = self.object.user_expenses.map{|expense| expense.cost}.sum
+    (self.object.balance - expenses).truncate(2)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
+  resources :user_expenses, only: [:index, :create, :update, :destroy]
+  resources :users, only: [:index, :update, :destroy]
+
+  #check out for frontend fetches https://learning.flatironschool.com/courses/4231/pages/authenticating-users?module_item_id=311665
+
+  #for login 
+  post "/login", to: "sessions#create"
+  #create new user
+  post "/signup", to: "users#create"
+  #allows user to stay logged in 
+  get "/me", to: "users#show"
+  #to log the user out 
+  delete "/logout", to: "sessions#destroy"
+
+  #for balance update
+  put "/balance", to: "users#update_balance"
+
+  #for remainder update
+  put "/remainder", to: "users#update_remainder"
   
-  resources :user_expenses
-  resources :users
-  resources :transferrable_transactions
+  resources :transactors
+  resources :transactees
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
